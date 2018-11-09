2f77e98b42cf3954aec58722ffc7fa468175a9e5
Tim Gagliano <timgagliano@enzombpro.local>
2018-11-09 13:39:33 -0600

    Initial Commit

---
+++ Gonna Do/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gonna Do
+//
+//  Created by Tim Gagliano on 11/9/18.
+//  Copyright © 2018 Tim Gagliano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
